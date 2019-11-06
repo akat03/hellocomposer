93740bf55c74693663e24246284a9c40e8c92bab
econosys-system <econosys.system@gmail.com>
2019-11-06 16:07:38 +0900

    [add] add files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hellocomposer
+
 HelloComposer
---
+++ src/HelloComposer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace akat03\HelloComposer;
+
+class HelloComposer
+{
+    public static function echo( $message='hello !' )
+    {
+    	echo $message;
+    }
+}
+
+
+
